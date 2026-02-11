c2c53a0558f8ffe28ecb55bd6858abd0e57d1a2f
Bhagwat-io <bhagwatkolekar8664@gmail.com>
2026-02-11 14:22:59 +0530

    Added Terraform Modules

---
+++ Prod-Env/terraform.tfvars
@@ -0,0 +1,119 @@
+# Store Variable Values
+
+# =========================
+# VPC Variable Values
+# =========================
+
+vpc_cidr_block = "10.0.0.0/16"
+vpc_tenancy = "default"
+vpc_name = "Bhagwat-prod-env-bhagwat-vpc"
+prod_env_region = "us-east-2"
+
+# =========================
+# public subnet Variable Values
+# =========================
+
+public_cidr_block = "10.0.1.0/24"
+public_subnet_availability_zone = "us-east-2a" 
+public_subnet_map_public_ip_on_launch = true
+public_subnet_name = "Bhagwat-prod-env--pub-subnet"
+
+# =========================
+# Private Subnet Variable Values
+# =========================
+
+private_cidr_block                 = "10.0.2.0/24"
+private_subnet_availability_zone   = "us-east-2b"
+private_subnet_name                = "Bhagwat-prod-env-pvt-subnet"
+
+# =========================
+# Internet Gateway Variable Values
+# =========================
+
+internet_gateway_name = "Bhagwat-prod-env-igw"
+
+
+# =========================
+# Public Route Table Variable Values
+# =========================
+
+public_route_cidr_block     = "0.0.0.0/0"
+public_route_table_name     = "Bhagwat-prod-env-public-route-table"
+
+
+# =========================
+# Elastic IP Variable Values
+# =========================
+
+eip_domain = "vpc"
+eip_name   = "Bhagwat-prod-env-eip"
+
+# =========================
+# NAT Gateway Variable Values
+# =========================
+
+nat_gateway_name = "Bhagwat-prod-env-nat-gw"
+
+
+# =========================
+# Private Route Table Variable Values
+# =========================
+
+private_route_cidr_block   = "0.0.0.0/0"
+private_route_table_name   = "Bhagwat-prod-env-private-route-table"
+
+
+# =========================
+# Key Pair Variable Values
+# =========================
+
+key_pair_name = "Bhagwat-prod-env-key"
+
+public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3CyJ62nwNuBDKFXrXVySTGBBGaPiaklF0I+izswjZz/0dXzPvmtQLBK21tQyWBHB9w8mXaQrRuuVG94B+jmJ06aonNgFfGM5of7GIgf2bUjlsj7cnNLjUBtZTI4V897nUMGwt0i1r5iEmDTNVYG7yEIhMBguV2IxveY7cqPdECOgqulq+If4ZjviXVgwZhKnJ9/XATlxYmQx5G++P2xrSHgZv51zJiM3l/F8C8r1mShV15A7Pyv4JjHPU9DS8wV6l0Jy6VOq9HPB9DvSldPTHjUhf8SkupZQuX7r4MWszuMtlCk4Z0qmOoAsNiAsWVHAOl+3hhjNdMdGzpXbwG88UXQlUzVrSLDH7ydDfZc/06eDzzqnSRmODzAr5VuXqCn1OnC6+hBHgxeE3fsMU/4k2E+G8uIPHZ2ZNTbgg78gjzDG6ES07dj9SnJa9uA+dtK8YqQlwpRKKCodzK4WUTfS8Oa9t2djj4JKY1nhP/nwIIMjHTzEmsFExHG8daMHilagYoU7CkTZoyto8zCieilLXi7QicRQ1vGV76ImlXp7GCmjMo/YlqzS+teuUXl3BtFZA6137xtNHZOmLlwb5XOzMrAxrVwdYqqn8aPG71V/ccI7/VJICJE6ZLTzLDVnNIfrZ9NhwQv6Fyj6zw5DlRwzuyjc2vLchkiieHSm8JJuCew== bhagwat@LAPTOP-91GIVD30"
+
+# =========================
+# Security Group Variable Values
+# =========================
+
+security_group_name        = "Bhagwat-prod-env-sg"
+security_group_description = "Allow SSH and application ports"
+
+ssh_port   = "22"
+app_port_1 = "8080"
+app_port_2 = "9000"
+ingress_protocol = "tcp"
+
+security_group_ingress_cidr = ["0.0.0.0/0"]
+security_group_egress_cidr  = ["0.0.0.0/0"]
+
+# Egress (Allow all outbound)
+egress_from_port   = "0"
+egress_to_port     = "0"
+egress_protocol    = "-1"
+egress_cidr_blocks = ["0.0.0.0/0"]
+
+# =========================
+# EC2 Public Instance Values
+# =========================
+
+public_instance_ami   = "ami-06e3c045d79fd65d9"
+public_instance_type = "t3.medium"
+public_instance_az   = "us-east-2a"
+
+associate_public_ip   = true
+public_instance_count = 1
+
+public_instance_name = "Bhagwat-prod-env-public-ec2"
+
+
+# =========================
+# EC2 Private Instance Values
+# =========================
+
+private_instance_ami   = "ami-06e3c045d79fd65d9"
+private_instance_type = "t2.micro"
+private_instance_az   = "us-east-2b"
+
+associate_public_ip_private_instance   = false
+
+private_instance_name = "Bhagwat-prod-env-private-ec2"
---
+++ Prod-Env/variables.tf
@@ -0,0 +1,237 @@
+# =========================
+# VPC Variables
+# =========================
+
+variable "vpc_cidr_block" {
+    description = "This is for vpc cidr block"
+}
+
+variable "vpc_tenancy" {
+    description = "This is tenancy for vpc"
+}
+
+variable "vpc_name" {
+    description = "This is tag name for vpc"
+}
+
+variable "prod_env_region" {
+    description = "This is for prod env vpc"
+}
+
+# =========================
+# Public subnet Variables
+# =========================
+
+variable "public_cidr_block" {
+    description = "This is for public subnet cidr"
+}
+
+variable "public_subnet_availability_zone" {
+    description = "This is for public subnet availability zone"
+}
+
+variable "public_subnet_map_public_ip_on_launch" {
+    description = "This is for public subnet"
+}
+
+variable "public_subnet_name" {
+    description = "This is tag name for public subnet"
+}
+
+# =========================
+# Private Subnet Variables
+# =========================
+
+variable "private_cidr_block" {
+    description = "This is for private subnet cidr"
+}
+
+variable "private_subnet_availability_zone" {
+    description = "This is for private subnet availability zone"
+}
+
+variable "private_subnet_name" {
+    description = "This is tag name for private subnet"
+}
+
+# =========================
+# Internet Gateway Variables
+# =========================
+
+variable "internet_gateway_name" {
+    description = "Tag name for Internet Gateway"
+}
+
+# =========================
+# Public Route Table Variables
+# =========================
+
+variable "public_route_cidr_block" {
+    description = "CIDR block for public route (internet access)"
+}
+
+variable "public_route_table_name" {
+    description = "Tag name for public route table"
+}
+
+variable "public_route_table_association" {
+    description = "Rout Table Association for public subnet"
+
+}
+
+
+# =========================
+# Elastic IP Variables
+# =========================
+
+variable "eip_domain" {
+    description = "Domain for Elastic IP (vpc)"
+}
+
+variable "eip_name" {
+    description = "Tag name for Elastic IP"
+}
+
+# =========================
+# NAT Gateway Variables
+# =========================
+
+variable "nat_gateway_name" {
+    description = "Tag name for NAT Gateway"
+}
+
+
+# =========================
+# Private Route Table Variables
+# =========================
+
+variable "private_route_cidr_block" {
+    description = "CIDR block for private route (internet via NAT)"
+}
+
+variable "private_route_table_name" {
+    description = "Tag name for private route table"
+}
+
+variable "private_route_table_association" {
+    description = "Rout Table Association for private subnet"
+
+}
+
+# =========================
+# Key Pair Variables
+# =========================
+
+variable "key_pair_name" {
+    description = "Name of the EC2 key pair"
+}
+
+variable "public_key" {
+    description = "Public SSH key material"
+}
+
+
+# =========================
+# Security Group Variables
+# =========================
+
+variable "security_group_name" {
+    description = "Name of the security group"
+}
+
+variable "security_group_description" {
+    description = "Description of the security group"
+}
+
+variable "ssh_port" {
+    description = "SSH port"
+}
+
+variable "app_port_1" {
+    description = "Application port 1"
+}
+
+variable "app_port_2" {
+    description = "Application port 2"
+}
+
+variable "security_group_ingress_cidr" {
+    description = "Allowed ingress CIDR blocks"
+}
+
+variable "security_group_egress_cidr" {
+    description = "Allowed egress CIDR blocks"
+}
+
+variable "ingress_protocol" {
+    description = "Allowed egress CIDR blocks"
+}
+
+variable "egress_from_port" {
+    description = "Egress from port"
+}
+
+variable "egress_to_port" {
+    description = "Egress to port"
+}
+
+variable "egress_protocol" {
+    description = "Egress protocol"
+}
+
+variable "egress_cidr_blocks" {
+    description = "Allowed egress CIDR blocks"
+}
+
+# =========================
+# EC2 Public Instance Variables
+# =========================
+
+variable "public_instance_ami" {
+    description = "AMI ID for public EC2 instance"
+}
+
+variable "public_instance_type" {
+    description = "Instance type"
+}
+
+variable "public_instance_az" {
+    description = "Availability Zone for public EC2"
+}
+
+variable "associate_public_ip" {
+    description = "Associate public IP to instance"
+}
+
+variable "public_instance_count" {
+    description = "Number of public EC2 instances"
+}
+
+variable "public_instance_name" {
+    description = "Name tag for public EC2"
+}
+
+
+# =========================
+# EC2 Private Instance Variables
+# =========================
+
+variable "private_instance_ami" {
+    description = "AMI ID for private EC2 instance"
+}
+
+variable "private_instance_type" {
+    description = "Instance type for private EC2"
+}
+
+variable "associate_public_ip_private_instance" {
+    description = "Associate public IP to instance"
+}
+
+variable "private_instance_az" {
+    description = "Availability Zone for private EC2"
+}
+
+variable "private_instance_name" {
+    description = "Name tag for private EC2"
+}
